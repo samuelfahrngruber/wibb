b0b6315f4f88bfe702f207b5dc684f73629873f3
samuelfahrngruber <35682879+samuelfahrngruber@users.noreply.github.com>
2020-03-17 18:36:36 +0100

    added new beers

--- wibb-db/mongodb-script-wibb.js
+++ wibb-db/mongodb-script-wibb.js
@@ -7,6 +7,8 @@ db.beers.insert({ name: "Stiegl", icon: "/res/img/beer/stiegl.png" });
 db.beers.insert({ name: "Gösser", icon: "/res/img/beer/goesser.png" });
 db.beers.insert({ name: "Hirter", icon: "/res/img/beer/hirter.png" });
 db.beers.insert({ name: "Villacher", icon: "/res/img/beer/villacher.png" });
+db.beers.insert({ name: "Puntigamer", icon: "/res/img/beer/puntigamer.png" });
+db.beers.insert({ name: "Murauer", icon: "/res/img/beer/murauer.png" });
 
 db.stores.drop();
 
@@ -16,6 +18,8 @@ db.stores.insert({ name: "Hofer", icon: "/res/img/store/hofer.png" });
 db.stores.insert({ name: "Merkur", icon: "/res/img/store/merkur.png" });
 db.stores.insert({ name: "MPreis", icon: "/res/img/store/mpreis.png" });
 db.stores.insert({ name: "Spar", icon: "/res/img/store/spar.png" });
+db.stores.insert({ name: "Eurospar", icon: "/res/img/store/eurospar.png" });
+db.stores.insert({ name: "Interspar", icon: "/res/img/store/interspar.png" });
 db.stores.insert({ name: "T&G", icon: "/res/img/store/tundg.png" });
 
 db.offers.drop();
