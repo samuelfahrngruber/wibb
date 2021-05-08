b3f760e2dd6380c7a4759bbbbd343360b7219363
Samuel Fahrngruber <samuel.fahrngruber@gmail.com>
2021-05-08 11:57:17 +0200

    fix(web): fixed index.html not displaying properly

--- wibb-ws-j/src/main/resources/public/script-about.js
+++ wibb-ws-j/src/main/resources/public/script-about.js
@@ -6,14 +6,14 @@ $(document).ready(function(){
         data.forEach(e => {
             delete e._id; // muss nicht
             beers.push(e);
-            $("#list-beers").append('<li class="list-group-item list-group-item-action horizontal-list-item-content"><img class="w-100" src="' + e.icon.imgUrl + '"><p class="text-center lead">' + e.name + '</p></li>');
+            $("#list-beers").append('<li class="list-group-item list-group-item-action horizontal-list-item-content"><img class="w-100" src="' + e.icon + '"><p class="text-center lead">' + e.name + '</p></li>');
         });
     });
     $.get("api/stores", function(data, status){
         data.forEach(e => {
             delete e._id; // muss nicht
             stores.push(e);
-            $("#list-stores").append('<li class="list-group-item list-group-item-action horizontal-list-item-content"><img class="w-100" src="' + e.icon.imgUrl + '"><p class="text-center lead">' + e.name + '</p></li>');
+            $("#list-stores").append('<li class="list-group-item list-group-item-action horizontal-list-item-content"><img class="w-100" src="' + e.icon + '"><p class="text-center lead">' + e.name + '</p></li>');
         });
     });
 });
